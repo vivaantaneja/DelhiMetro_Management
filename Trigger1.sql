2c6bcdafc84295253c19c648b81e0feb3d283a66
AryanJulka <97023709+AryanJulka@users.noreply.github.com>
2023-04-28 04:20:08 +0530

    Update Trigger1.sql

--- Trigger1.sql
+++ Trigger1.sql
@@ -1,11 +1,32 @@
 /*Trigger on TrainSchedule table to prevent a new schedule from being added if the departure time is earlier than the arrival time:*/
 
 CREATE OR REPLACE TRIGGER tr_schedule_check_time
-BEFORE INSERT OR UPDATE ON TrainSchedule
-FOR EACH ROW
-BEGIN
-  IF :NEW.DepartureTime <= :NEW.ArrivalTime THEN
-    RAISE_APPLICATION_ERROR(-20001, 'Departure time must be later than arrival time.');
-  END IF;
+FOR INSERT OR UPDATE ON TrainSchedule
+COMPOUND TRIGGER
+  v_error NUMBER;
+  
+  BEFORE STATEMENT IS
+  BEGIN
+    v_error := 0;
+  END BEFORE STATEMENT;
+  
+  BEFORE EACH ROW IS
+  BEGIN
+    IF :NEW.DepartureTime <= :NEW.ArrivalTime THEN
+      v_error := 1;
+    END IF;
+  END BEFORE EACH ROW;
+  
+  AFTER EACH ROW IS
+  BEGIN
+    IF v_error = 1 THEN
+      RAISE_APPLICATION_ERROR(-20001, 'Departure time must be later than arrival time.');
+    END IF;
+  END AFTER EACH ROW;
+  
+  AFTER STATEMENT IS
+  BEGIN
+    v_error := 0;
+  END AFTER STATEMENT;
 END;
 /
