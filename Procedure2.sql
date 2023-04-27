426c60f4fa5520a84f6c44a2b16db40917a67033
vivaantaneja <96987132+vivaantaneja@users.noreply.github.com>
2023-04-28 04:10:49 +0530

    proc 2 update

---
+++ Procedure2.sql
@@ -0,0 +1,18 @@
+/*Procedure to insert a new complaint:*/
+
+CREATE SEQUENCE complaint_seq START WITH 1 INCREMENT BY 1; /*Create Sequence First*/
+CREATE OR REPLACE PROCEDURE add_complaint(
+  passenger_id IN NUMBER,
+  station_id IN NUMBER,
+  train_id IN NUMBER,
+  complaint_type IN VARCHAR2,
+  complaint_desc IN VARCHAR2
+)
+IS
+BEGIN
+  INSERT INTO complaints(complaintid, passengerid, stationid, trainid, complainttype, complaintdesc, complaintdate)
+  VALUES (complaint_seq.NEXTVAL, passenger_id, station_id, train_id, complaint_type, complaint_desc, SYSDATE);
+  
+  DBMS_OUTPUT.PUT_LINE('New complaint added');
+END;
+/
